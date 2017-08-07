b184fff7e8fd5ca00d276110c9f382f4369974b6
Martynas Pocius <Martasp3289@gmail.com>
2017-08-07 17:04:22 +0300

    Added new positions

---
+++ RT/App5/App5/Sevices/Geolocation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Position = Plugin.Geolocator.Abstractions.Position;
+
+namespace app5.Sevices
+{
+    public static class Geolocation
+    {
+        public static Position Position { get; set; }
+        public static IGeolocator locator;
+
+        static Geolocation()
+        {
+             GetPositionAsync();
+            //locator.PositionChanged += LocatorOnPositionChanged;
+        }
+
+        public static async Task<Position> GetPositionAsync()
+        {
+            try
+            {
+                if (Position != null)
+                {
+
+                    return Position;
+                }
+                locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
+                //return await locator.GetPositionAsync(10000);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+            return null;
+        }
+
+        private static void LocatorOnPositionChanged(object sender, PositionEventArgs positionEventArgs)
+        {
+
+            Position = positionEventArgs.Position;
+        }
+    }
+}
--- RT/App5/App5/Views/MapPage.cs
+++ RT/App5/App5/Views/MapPage.cs
@@ -1,72 +1,86 @@
-﻿using System;
+﻿using System.Diagnostics;
 using Plugin.Geolocator;
-using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
-
+using Position = Xamarin.Forms.Maps.Position;
+using PositionGEO = Plugin.Geolocator.Abstractions.Position;
 namespace app5.Views
 {
-
-
-
     public class MapPage : ContentPage
     {
         public Map map { get; set; }
+        public IGeolocator locator { get; set; }
+        public PositionGEO Position { get; set; }
         public MapPage()
         {
             map = new Map(
                 MapSpan.FromCenterAndRadius(
                     new Position(55.7565436, 21.8992711 ), Distance.FromMeters(605)));
             Content = map;
+            addPins();
+            AddMy();
             map.MapType = MapType.Satellite;
+
+        }
+
+        public async void AddMy()
+        {
+            locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 5;
+            locator.PositionChanged += position_Changed;
+            await locator.StartListeningAsync(minTime: 2000, minDistance: 50);
+        }
+
+        // Callback function for when GPS location changes
+        void position_Changed(object obj, PositionEventArgs e)
+        {
+            updateGPSDataList(e.Position);
+            var pin2 = new Pin()
+            {
+                Type = PinType.Place,
+                Position = new Position(e.Position.Latitude, e.Position.Longitude),
+                Label = "shop1!"+e.Position.Latitude,
+                Address = "addres1"+ e.Position.Longitude
+
+            };
+            map.Pins.Add(pin2);
+            RefreshMap(e.Position);
+            DisplayAlert("ok", locator.DesiredAccuracy.ToString(), "ok");
+        }
+
+        // Update GPS location displays and database
+        private void updateGPSDataList(PositionGEO position)
+        {
+            Debug.WriteLine("Position changed: " + position.Latitude);
+        }
+
+        public void RefreshMap(PositionGEO position)
+        {
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude), Xamarin.Forms.Maps.Distance.FromMeters(605)));
+            // locator.DesiredAccuracy = locator.DesiredAccuracy / 2;
+           // map. .RouteCoordinates.Add(new Position(37.776831, -122.394627));
+        }
+        public void  addPins()
+        {
             var pin1 = new Pin()
             {
-                
+
                 Position = new Position(55.7515436, 21.8992711),
-                Label = "Kababine!",
-                Address = "kašakas"
+                Label = "Shop1!",
+                Address = "addres1"
 
             };
             var pin2 = new Pin()
             {
                 Type = PinType.Place,
                 Position = new Position(55.7515436, 21.8982711),
-                Label = "Arbuzine!",
-                Address = "dod"
+                Label = "Shop2!",
+                Address = "addres2"
 
             };
-
-            Get();
             map.Pins.Add(pin1);
             map.Pins.Add(pin2);
-        }
-
-        public async void Get()
-        {
-            try
-            {
-                var locator = CrossGeolocator.Current;
-                locator.DesiredAccuracy = 10;
-                var position = await locator.GetPositionAsync(10000);
-
-                if (position != null)
-                {
-                    DisplayAlert("get", position.ToString(), "OK");
-                    map.Pins.Add(new Pin()
-                    {
-                        Type = PinType.Place,
-                        Position = new Position(position.Latitude, position.Longitude),
-                        Label = "Aš!",
-                        Address = "cia"
-
-                    });
-                    map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude),Distance.FromMeters(605)));
-                }
-            }
-            catch (Exception e)
-            {
-                DisplayAlert("Error", e.ToString(), "OK");
-            }     
         }
     }
 }
